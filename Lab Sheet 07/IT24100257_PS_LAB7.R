ddb21f8e96b58e38017ff8ae9ce9a6d16657fef7
IT24100257 <166105809+kavindusachinthana@users.noreply.github.com>
2025-09-16 23:48:11 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100257_PS_LAB7.R
@@ -0,0 +1,14 @@
+setwd("D:\\SLIIT LECTURES\\Y2 S1\\Lab sheets & answers\\PS (IT2120)")
+getwd()
+
+#1
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+#2
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+#3
+1 - pnorm(130, mean=100, sd=15)
+
+qnorm(0.95, mean=100, sd=15)
+
